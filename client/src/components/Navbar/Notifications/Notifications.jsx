62ba595b0c2be4ed6812b9b73ee7be5956a601ea
AbhishekSagu <abhisheksagu@gmail.com>
2023-02-13 13:16:07 +0530

    added notifications component

--- client/src/components/Navbar/Notifications/Notifications.jsx
+++ client/src/components/Navbar/Notifications/Notifications.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import Style from './Notifications.module.css';
+import images from '../../../img';
 
 const Notifications = () => {
     return (
-        <div>Notifications</div>
+        <div className={Style.notification}>
+            <p>Notification</p>
+            <div className={Style.notification_box}>
+                <div className={Style.notification_box_img}>
+                    <Image
+                        src={images.user1}
+                        alt="profile image"
+                        width={50}
+                        height={50}
+                        className={Style.notification_box_img}
+                    />
+
+                </div>
+
+                <div className={Style.notification_box_info}>
+                    <h4>Shisuke Urahara</h4>
+                    <p>Measure your action User .....</p>
+                    <small>3 minutes ago</small>
+                </div>
+
+                <span className={Style.notification_box_new}></span>
+
+
+            </div>
+
+        </div>
     )
 }
 
