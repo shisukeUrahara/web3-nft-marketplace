8ee3c54ad721eac055f24010951065323d9b90d5
AbhishekSagu <abhisheksagu@gmail.com>
2023-02-13 13:01:45 +0530

    added helpcenter component

--- client/src/components/Navbar/HelpCenter/HelpCenter.jsx
+++ client/src/components/Navbar/HelpCenter/HelpCenter.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React from 'react';
+import Style from './HelpCenter.module.css';
+import Link from 'next/link';
 
 const HelpCenter = () => {
+  const helpCenter = [
+    {
+      name: "About",
+      link: "aboutus",
+    },
+    {
+      name: "Contact Us",
+      link: "contactus",
+    },
+    {
+      name: "Sign Up",
+      link: "signUp",
+    },
+    {
+      name: "LogIn",
+      link: "login",
+    },
+    {
+      name: "Subscription",
+      link: "subscription",
+    },
+  ];
+
+
   return (
-    <div>HelpCenter</div>
+    <div className={Style.box}>
+      {
+        helpCenter.map((item, index) => (
+          <div className={Style.helpCenter} key={index + 1}>
+            <Link href={{ pathname: `${item.link}` }}>
+              {item.name}
+            </Link>
+
+          </div>
+
+        ))
+      }
+
+    </div>
   )
 }
 
