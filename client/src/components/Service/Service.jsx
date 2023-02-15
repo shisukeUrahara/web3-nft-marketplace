d7824d1ae8ae576f58960912e0300cbf1acd83c0
AbhishekSagu <abhisheksagu@gmail.com>
2023-02-15 12:52:43 +0530

    styled service component

--- client/src/components/Service/Service.jsx
+++ client/src/components/Service/Service.jsx
@@ -22,7 +22,7 @@ const Service = () => {
                 <div className={Style.service_box_item}>
                     <Image src={images.service2} alt="Filter & Discover" width={100} height={100} />
                     <p className={Style.service_box_item_step}>
-                        <span>Step 1</span>
+                        <span>Step 2</span>
                     </p>
                     <h3>Filter & Discover</h3>
                     <p>
@@ -34,7 +34,7 @@ const Service = () => {
                 <div className={Style.service_box_item}>
                     <Image src={images.service3} alt="Connect Wallet" width={100} height={100} />
                     <p className={Style.service_box_item_step}>
-                        <span>Step 1</span>
+                        <span>Step 3</span>
                     </p>
                     <h3>Connect Wallet</h3>
                     <p>
