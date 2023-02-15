c0feb3ba5a6be82035e3e03cc5672f3e3da36057
AbhishekSagu <abhisheksagu@gmail.com>
2023-02-15 20:10:22 +0530

    styled category component

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -54,35 +54,37 @@ const Category = () => {
     ];
 
     return (
-        <div className={Style.category}>
-            {
-                CategoryArray.map((item, index) => (
-                    <div className={Style.category_box} key={index + 1}>
-                        <Image
-                            src={item.images}
-                            alt="Background image"
-                            className={Style.category_box_img}
-                            width={350}
-                            height={150}
-                        />
+        <div className={Style.category_container}>
+            <div className={Style.category}>
+                {
+                    CategoryArray.map((item, index) => (
+                        <div className={Style.category_box} key={index + 1}>
+                            <Image
+                                src={item.images}
+                                alt="Background image"
+                                className={Style.category_box_img}
+                                width={350}
+                                height={150}
+                            />
 
-                        <div className={Style.category_box_title}>
-                            <span>
-                                <BsCircleFill />
-                            </span>
+                            <div className={Style.category_box_title}>
+                                <span>
+                                    <BsCircleFill />
+                                </span>
 
-                            <div className={Style.category_box_title_info}>
-                                <h4>Entertainment</h4>
-                                <small>{item.name}</small>
+                                <div className={Style.category_box_title_info}>
+                                    <h4>Entertainment</h4>
+                                    <small>{item.name}</small>
+
+                                </div>
 
                             </div>
 
                         </div>
+                    ))
+                }
 
-                    </div>
-                ))
-            }
-
+            </div>
         </div>
     )
 }
