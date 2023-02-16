35bd68c0ac5311d330f5495b7388acf9da8558e5
AbhishekSagu <abhisheksagu@gmail.com>
2023-02-16 19:12:57 +0530

    added followerTabCard component

--- client/src/components/FollowerTab/FollowerTabCard/FollowerTabCard.jsx
+++ client/src/components/FollowerTab/FollowerTabCard/FollowerTabCard.jsx
@@ -1,10 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MdVerified } from 'react-icons/md';
+import Image from 'next/image';
+import { TiTick } from 'react-icons/ti';
 
 import Style from './FollowerTabCard.module.css';
+import images from '../../../img';
 
-const FollowerTabCard = () => {
+const FollowerTabCard = ({ item, index }) => {
+    const [following, setFollowing] = useState(false);
+
     return (
-        <div>FollowerTabCard</div>
+        <div className={Style.followerTabCard}>
+            <div className={Style.followerTabCard_rank}>
+                <p>#{index + 1} <span>🥇</span></p>
+
+            </div>
+
+            <div className={Style.followerTabCard_box}>
+                <div className={Style.followerTabCard_box_img}>
+                    <Image
+                        src={images.creatorbackground1}
+                        alt="profile background"
+                        width={500}
+                        height={300}
+                        className={Style.followerTabCard_box_img_img}
+                    />
+                </div>
+
+                <div className={Style.followerTabCard_box_profile}>
+                    <Image
+                        className={Style.followerTabCard_box_profile_img}
+                        src={images.user1}
+                        alt="profile picture"
+                        width={50}
+                        height={50}
+                    />
+
+                </div>
+
+                <div className={Style.followerTabCard_box_info}>
+                    <div className={Style.followerTabCard_box_info_name}>
+                        <h4>John Davis {" "}
+                            <span>
+                                <MdVerified />
+                            </span>
+                        </h4>
+                        <p>1.234 ETH</p>
+                    </div>
+                    <div className={Style.followerTabCard_box_info_following}>
+                        {
+                            !following ? (
+                                <a onClick={() => setFollowing(true)}>Follow{" "}{" "} <span><TiTick /></span></a>
+                            ) : (
+                                <a onClick={() => setFollowing(false)}>Following{" "}{" "}</a>
+                            )
+                        }
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
