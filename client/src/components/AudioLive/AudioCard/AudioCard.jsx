24d96da9076a67f05b208033028f99ab319252de
AbhishekSagu <abhisheksagu@gmail.com>
2023-02-17 11:07:17 +0530

    added audioCard component

--- client/src/components/AudioLive/AudioCard/AudioCard.jsx
+++ client/src/components/AudioLive/AudioCard/AudioCard.jsx
@@ -13,7 +13,69 @@ const AudioCard = () => {
 
     return (
         <div className={Style.audioCard}>
+            <div className={Style.audioCard_box}>
+                <div className={Style.audioCard_box_like_time}>
+                    <div className={Style.audioCard_box_like} onClick={() => setLike((prevValue) => !prevValue)}>
+                        {
+                            like ? (
+                                <AiFillHeart className={Style.audioCard_box_like_icon} />
+                            ) : (
+                                <AiOutlineHeart className={Style.audioCard_box_like_icon_unlike} />
+                            )
+                        }
+                        <span>24</span>
+                    </div>
 
+                    <div className={Style.audioCard_box_time}>
+                        <div className={Style.audioCard_box_like_time_remaining}>
+                            <span>Remaining</span>
+                            <h5>3h : 15m : 20s</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={Style.audioCard_box_player}>
+                    <Image src={images.musiceWave} alt="music" width={200} objectFit="cover" />
+                    <div className={Style.audioCard_box_musicPlayer} onClick={() => setPlay((prevValue) => !prevValue)}>
+                        {
+                            play ? (
+                                <div className={Style.audioCard_box_musicPlayer_icon}>
+                                    <TbPlayerPause />
+                                </div>
+                            ) : (
+                                <div className={Style.audioCard_box_musicPlayer_icon}>
+                                    <TbPlayerPlay />
+                                </div>
+                            )
+                        }
+                    </div>
+                </div>
+
+                <div className={Style.audioCard_box_details}>
+                    <div className={Style.audioCard_box_details_info}>
+                        <h4>Music NFT #1345</h4>
+                        <div className={Style.audioCard_box_details_info_price}>
+                            <small>Price</small>
+                            <p>$3221.123</p>
+                        </div>
+                    </div>
+
+                    <div className={Style.audioCard_box_details_stock}>
+                        <small>24 in stock</small>
+                    </div>
+                </div>
+
+                <div className={Style.audioCard_box_img}>
+                    <Image
+                        src={images.creatorbackground10}
+                        alt="Creator background"
+                        width={500}
+                        height={500}
+                        objectFit="cover"
+                    />
+
+                </div>
+            </div>
         </div>
     )
 }
