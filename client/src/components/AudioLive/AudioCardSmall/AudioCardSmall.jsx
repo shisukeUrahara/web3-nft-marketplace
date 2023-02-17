0eb4bace2fb8cbec9f77c3dd37aec7e635728b12
AbhishekSagu <abhisheksagu@gmail.com>
2023-02-17 11:20:03 +0530

    added audioCardSmall component

--- client/src/components/AudioLive/AudioCardSmall/AudioCardSmall.jsx
+++ client/src/components/AudioLive/AudioCardSmall/AudioCardSmall.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Image from 'next/legacy/image';
+import { TbPlayerPlay, TbPlayerPause } from 'react-icons/tb';
+
+
+import Style from './AudioCardSmall.module.css';
+import images from '../../../img'
 
 const AudioCardSmall = () => {
+    const [play, setPlay] = useState(false);
+
     return (
-        <div>AudioCardSmall</div>
+        <div className={Style.audioPlayer}>
+            <div className={Style.audioPlayer_box}>
+                <Image
+                    src={images.creatorbackground1}
+                    alt="music"
+                    width={100}
+                    height={100}
+                    className={Style.audioPlayer_box_img}
+                    objectFit="cover"
+                />
+
+                <div className={Style.audioPlayer_box_info}>
+                    <h4>NFT Music #1234</h4>
+                    <div className={Style.audioPlayer_box_info_box}>
+                        {/* <LikeProfile /> */}
+                        <div className={Style.audioPlayer_box_info_box_price}>
+                            <small>Price</small>
+                            <p>1.00 ETH</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className={Style.audioPlayer_box_playBtn} onClick={() => setPlay((prevValue) => !prevValue)}>
+                    {
+                        play ? (
+                            <TbPlayerPause />
+                        ) : (
+                            <TbPlayerPlay />
+                        )
+                    }
+
+
+                </div>
+
+            </div>
+        </div>
     )
 }
 
