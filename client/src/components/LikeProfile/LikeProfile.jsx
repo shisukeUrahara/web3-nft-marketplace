2888b14be4f10471b422acadd9ca5599c3dafaa0
AbhishekSagu <abhisheksagu@gmail.com>
2023-02-17 11:57:41 +0530

    fixed issues

--- client/src/components/LikeProfile/LikeProfile.jsx
+++ client/src/components/LikeProfile/LikeProfile.jsx
@@ -5,7 +5,7 @@ import Style from './LikeProfile.module.css';
 import images from '../../img';
 
 const LikeProfile = () => {
-    const imageArray = [1, 2, 3, 4];
+    const imageArray = [images.user1, images.user2, images.user3, images.user4];
 
     return (
         <div className={Style.like}>
@@ -13,7 +13,7 @@ const LikeProfile = () => {
                 imageArray.map((item, index) => (
                     <div className={Style.like_box} key={index + 1}>
                         <Image
-                            src={images.user1}
+                            src={item}
                             width={15}
                             height={15}
                             objectFit="cover"
