8e432a153f238592c579270a0bc496b919eb0106
AbhishekSagu <abhisheksagu@gmail.com>
2023-02-22 11:30:10 +0530

    added deploy script for marketplace contract

--- Nft-MarketPlace-Contracts/hardhat.config.js
+++ Nft-MarketPlace-Contracts/hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+const dotenv= require("dotenv");
+dotenv.config();
+
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks:{
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    }
+  }
 };
